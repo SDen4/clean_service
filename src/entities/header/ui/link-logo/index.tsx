2b13a720d62cbdfe17616ed210f239e010efaf7e
SDen4 <asw325@yandex.ru>
2026-07-13 20:23:09 +0500

    refactor: logo image

--- src/entities/header/ui/link-logo/index.tsx
+++ src/entities/header/ui/link-logo/index.tsx
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 
 import { mainPageImport } from '@/entities/page';
 
+import logo from '@/shared/assets/logoCompressed.png';
 import { ROUTES } from '@/shared/config';
-
-import logo from '../../assets/logoCompressed.png';
 
 export const LinkLogo = () => (
   <Link to={ROUTES.MAIN} onMouseEnter={mainPageImport}>
