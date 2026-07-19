e9928ddac346a65b654bd882cb5dd222f0d6b560
SDen4 <asw325@yandex.ru>
2026-07-19 16:35:49 +0500

    feat: fallback styles

--- src/app/main-layout/fallback.tsx
+++ src/app/main-layout/fallback.tsx
@@ -1,1 +1,8 @@
-export const Fallback = () => <div>Loading...</div>; // TEMP!!!
+import { Loader } from 'lucide-react';
+
+export const Fallback = () => (
+  <div className="flex flex-col items-center w-full gap-10 min-h-[50vh]">
+    <Loader className="animate-spin w-10 h-10" />
+    <h4>Загрузка...</h4>
+  </div>
+);
