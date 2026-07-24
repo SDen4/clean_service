8713fd2b1e451ae5436fde1755ce9a4065156433
SDen4 <asw325@yandex.ru>
2026-07-24 21:39:11 +0500

    feat: services page

--- src/entities/text/ui/service-item/index.tsx
+++ src/entities/text/ui/service-item/index.tsx
@@ -1,7 +1,15 @@
 import { SquareCheckBig } from 'lucide-react';
 
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/shared/ui';
+
 interface IProps {
-  item: { text: string; icon?: React.ElementType };
+  item: { text: string; description?: string; icon?: React.ElementType };
 }
 
 /** Элемент текста списка услуг */
@@ -11,11 +19,25 @@ export const ServiceItem = ({ item }: IProps) => {
   const Icon = item.icon || SquareCheckBig;
 
   return (
-    <div className="flex gap-3">
-      <Icon className="pt-1 w-10 h-10 min-h-10 min-w-10" />
-      <h2 className="text-2xl sm:text-4xl text-sky-900 dark:text-blue-100">
-        {item.text}
-      </h2>
-    </div>
+    <Card className="relative min-h-[280px] min-w-[300px] w-[30%] justify-between group hover:(shadow-xl) hover:-translate-y-1 transition-all duration-[0.3s] bg-gradient-to-tl from-slate-100 to-slate-300">
+      <CardHeader>
+        <CardTitle className="z-[1] p-4 text-2xl text-sky-900 ">
+          {item.text}
+        </CardTitle>
+      </CardHeader>
+
+      <div>
+        {!!item.description && (
+          <CardDescription className="p-4">{item.description}</CardDescription>
+        )}
+        <CardFooter className="px-4 py-1">
+          <p>Что сюда добавить?</p>
+        </CardFooter>
+      </div>
+
+      <div className="absolute -bottom-12 -right-12">
+        <Icon className="w-64 h-64 min-h-10 opacity-35 group-hover:opacity-10 transition-all duration-[0.3s] group-hover:translate-x-16 group-hover:translate-y-16" />
+      </div>
+    </Card>
   );
 };
--- src/pages/services-page/ui/index.tsx
+++ src/pages/services-page/ui/index.tsx
@@ -1,9 +1,10 @@
 import {
   BadgeDollarSign,
-  BadgePercent,
   BrushCleaning,
   Cog,
-  HandPlatter,
+  Forklift,
+  ShoppingCart,
+  UserRoundCog,
   Users,
   Wrench,
 } from 'lucide-react';
@@ -12,21 +13,47 @@ import { ServiceItem } from '@/entities/text';
 
 const serviceList = [
   {
-    text: 'Ремонт и обслуживание поломоечных машин, промышленных и профессиональных пылесосов любых марок',
+    text: 'Ремонт и обслуживание поломоечных машин',
+    description: 'Любые торговые марки',
     icon: Wrench,
   },
-  { text: 'Продажа профессиональной химии и инвентаря', icon: BadgePercent },
-  { text: 'Продажа техники для клининга', icon: BadgeDollarSign },
   {
-    text: 'Услуги аренды уборочной техники (поломоечные машины, пылесосы, парогенераторы)',
-    icon: HandPlatter,
+    text: 'Ремонт и обслуживание промышленных и профессиональных пылесосов',
+    description: 'Любые торговые марки',
+    icon: UserRoundCog,
   },
   {
-    text: 'Профессиональная уборка: Торговые центры + Складские площадки + Гостиницы + Офисы + Производственные площадки',
+    text: 'Услуги аренды уборочной техники',
+    description: 'Поломоечные машины, пылесосы, парогенераторы',
+    icon: Forklift,
+  },
+  {
+    text: 'Подбор и продажа запчастей и комплектующих',
+    description: 'Добавить сюда торговые марки или описание',
+    icon: Cog,
+  },
+  {
+    text: 'Продажа профессиональной химии и инвентаря',
+    description: 'Добавить сюда торговые марки или описание',
+    icon: ShoppingCart,
+  },
+
+  {
+    text: 'Продажа техники для клининга',
+    description: 'Добавить сюда торговые марки или описание',
+    icon: BadgeDollarSign,
+  },
+  {
+    text: 'Профессиональная уборка',
+    description:
+      'Торговые центры, складские площадки, гостиницы, офисы, производственные площадки',
     icon: BrushCleaning,
   },
-  { text: 'Свои выездные бригады', icon: Users },
-  { text: 'Подбор и продажа запчастей и комплектующих', icon: Cog },
+  {
+    text: 'Свои выездные бригады',
+    description: 'Добавить сюда описание',
+    icon: Users,
+  },
 ];
 
 const ServicesPage = () => {
@@ -34,9 +61,11 @@ const ServicesPage = () => {
     <div className="flex flex-col gap-9">
       <h2 className="mb-2">Услуги</h2>
 
-      {serviceList.map((el) => (
-        <ServiceItem key={el.text} item={el} />
-      ))}
+      <div className="flex flex-wrap gap-12 justify-center">
+        {serviceList.map((el) => (
+          <ServiceItem key={el.text} item={el} />
+        ))}
+      </div>
     </div>
   );
 };
