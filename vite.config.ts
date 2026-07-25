c5ef423ea9c386bde6306815d5b0973b6d6e7811
SDen4 <asw325@yandex.ru>
2026-07-25 10:25:09 +0500

    feat: add rollup-plugin-visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/clean_service/',
-  plugins: [react()],
+  server: { open: true },
+  plugins: [
+    react(),
+    visualizer({ open: true, gzipSize: true, brotliSize: true }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
