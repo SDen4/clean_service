2c20a20ce72c7d35ac9ea76a0df128b270747b12
SDen4 <asw325@yandex.ru>
2026-08-18 20:43:50 +0500

    feat: add map draft

--- src/pages/contacts/ui/index.tsx
+++ src/pages/contacts/ui/index.tsx
@@ -10,8 +10,8 @@ import { PageWrapper } from '@/entities/page';
 
 import { Card, CardTitle } from '@/shared/ui';
 
-const cardStyles =
-  'flex flex-col gap-5 min-w-[220px] p-4 bg-gradient-to-tl from-slate-100 to-slate-300';
+const gradientStyle = 'bg-gradient-to-tl from-slate-100 to-slate-300';
+const cardStyles = `flex flex-col gap-5 min-w-[220px] p-4 bg-gradient-to-tl from-slate-100 to-slate-300 ${gradientStyle}`;
 
 const ContactsPage = () => {
   return (
@@ -44,6 +44,22 @@ const ContactsPage = () => {
 
           <MailItem mail={mail} />
         </Card>
+      </div>
+
+      <h2>Адрес</h2>
+
+      <div className={`rounded-xl w-full md:w-fit p-2 ${gradientStyle}`}>
+        <div className="overflow-hidden rounded-xl">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d2182.507943520939!2d60.5799851!3d56.8372307!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sru!4v1787066207054!5m2!1sru!2sru" // TEMP !!!
+            width="725"
+            height="450"
+            style={{ borderRadius: '8px' }}
+            loading="lazy"
+            allowTransparency
+            referrerPolicy="strict-origin-when-cross-origin"
+          />
+        </div>
       </div>
     </PageWrapper>
   );
